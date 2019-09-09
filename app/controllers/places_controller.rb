bb422b28ea8894b3a873d29163e0f71403703cf3
B. Kyrk Ullman <git@bkullman.com>
2019-09-09 20:44:33 +0000

    Form saves to db now

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -9,4 +9,14 @@ class PlacesController < ApplicationController
     @place = Place.new
   end
 
+  def create
+    Place.create(place_params)
+    redirect_to root_path
+  end
+
+  private
+
+  def place_params
+    params.require(:place).permit(:name, :desc, :add)
+  end
 end
