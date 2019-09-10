97669577746f2299d44599d8937384526eec4106
B. Kyrk Ullman <git@bkullman.com>
2019-09-10 10:44:57 +0000

    adding lat+lng

--- app/models/place.rb
+++ app/models/place.rb
@@ -1,5 +1,8 @@
 class Place < ApplicationRecord
     belongs_to :user
+    geocoded_by :add
+    after_validation :geocode
+
     validates :name, presence: true, length: { minimum: 3 }
     validates :add, presence: true
     validates :desc, presence: true
