c47fbd181e4b7ecb3ce5d5168e6ea2b6a9a559a4
B. Kyrk Ullman <git@bkullman.com>
2019-09-10 05:20:59 +0000

    adding users

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,4 +1,5 @@
 class PlacesController < ApplicationController
+  before_action :authenticate_user!, only: [:new, :create]
   include Pagy::Backend
 
   def index
@@ -10,7 +11,7 @@ class PlacesController < ApplicationController
   end
 
   def create
-    Place.create(place_params)
+    current_user.places.create(place_params)
     redirect_to root_path
   end
 
